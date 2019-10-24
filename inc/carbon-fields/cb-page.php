fb8ba50d0661185d6660e4057a1cdadc2209ad13
serii <seriiburduja@gmail.com>
2019-10-24 16:57:43 +0300

    where to buy page

--- inc/carbon-fields/cb-page.php
+++ inc/carbon-fields/cb-page.php
@@ -63,12 +63,39 @@ function crb_where_to_buy_page_title_options() {
 	Container::make( 'post_meta', __( 'Where to buy page title' ) )
 	         ->where( 'post_id', '=', 112 )
 	         ->or_where( 'post_id', '=', 107 )
-	         ->add_fields( array(
+	         ->add_tab( ( 'Page title' ), array(
 		         Field::make( 'text', 'crb_where_to_buy_title_ro', __( 'crb_where_to_buy_title_ro' ) )
 		              ->set_width( 30 ),
 		         Field::make( 'text', 'crb_where_to_buy_title_ru', __( 'crb_where_to_buy_title_ru' ) )
 		              ->set_width( 30 ),
 		         Field::make( 'text', 'crb_where_to_buy_title_en', __( 'crb_where_to_buy_title_en' ) )
 		              ->set_width( 30 ),
+
+	         ) )
+	         ->add_tab( ( 'Brand title' ), array(
+		         Field::make( 'text', 'crb_where_to_buy_page_brand_title_ro', __( 'crb_where_to_buy_page_brand_title_ro' ) )
+		              ->set_width( 30 ),
+		         Field::make( 'text', 'crb_where_to_buy_page_brand_title_ru', __( 'crb_where_to_buy_page_brand_title_ru' ) )
+		              ->set_width( 30 ),
+		         Field::make( 'text', 'crb_where_to_buy_page_brand_title_en', __( 'crb_where_to_buy_page_brand_title_en' ) )
+		              ->set_width( 30 ),
+
+		         Field::make( 'text', 'crb_where_to_buy_page_brand_subtitle_ro', __( 'crb_where_to_buy_page_brand_subtitle_ro' ) )
+		              ->set_width( 30 ),
+		         Field::make( 'text', 'crb_where_to_buy_page_brand_subtitle_ru', __( 'crb_where_to_buy_page_brand_subtitle_ru' ) )
+		              ->set_width( 30 ),
+		         Field::make( 'text', 'crb_where_to_buy_page_brand_subtitle_en', __( 'crb_where_to_buy_page_brand_subtitle_en' ) )
+		              ->set_width( 30 ),
+
+	         ) )
+	         ->add_tab( ( 'Short text' ), array(
+		         Field::make( 'textarea', 'crb_where_to_buy_page_short_text_ro', __( 'crb_where_to_buy_page_short_text_ro' ) )
+		              ->set_width( 30 ),
+		         Field::make( 'textarea', 'crb_where_to_buy_page_short_text_ru', __( 'crb_where_to_buy_page_short_text_ru' ) )
+		              ->set_width( 30 ),
+		         Field::make( 'textarea', 'crb_where_to_buy_page_short_text_en', __( 'crb_where_to_buy_page_short_text_en' ) )
+		              ->set_width( 30 ),
+
 	         ) );
+
 }
--- page-where-to-buy.php
+++ page-where-to-buy.php
@@ -4,38 +4,67 @@
  */
 ?>
 <?php get_header(); ?>
-    <section class="section section-where-to-buy"
-             style="background-image: url('assets/i/where_to_buy_background.jpg'); background-size: cover;">
-        <h2 class="section__title">Where to buy</h2>
-        <div class="blocks"><a class="blocks__item"
-                               href="https://www.brother-ism.com/products/industrial-sewing-machines.html">
-                <div class="blocks__image-bg" style="background-image: url('assets/i/blocks/1.jpg')"></div>
-                <div class="blocks__content">
-                    <header class="blocks__content-header"><img class="blocks__image_show"
-                                                                src="assets/i/blocks/block-arrow.png"><img
-                                class="blocks__image_hover" src="assets/i/blocks/block-arrow-hover.png">
-                        <div class="blocsk__titles">
-                            <h4 class="blocks__brand-title">Industrial</h4>
-                            <h3 class="blocks__title">Sewing machines</h3>
-                        </div>
-                    </header>
-                    <div class="blocks__text">Whether straight stitch, buttonholer or bar tack, Brother has the right
-                        sewing machine for your production line.
+<section class="section section-where-to-buy"
+         style="background-image: url('<?php echo get_template_directory_uri() ?>/site/assets/i/where_to_buy_background.jpg'); background-size: cover;">
+    <h2 class="section__title"><?php the_title(); ?></h2>
+
+    <div class="blocks">
+	    <?php $industrial_page_where_to_buy = new WP_Query([
+		    'page_id' => 107
+	    ]); ?>
+
+	    <?php if($industrial_page_where_to_buy->have_posts()): ?>
+		    <?php while($industrial_page_where_to_buy->have_posts()): ?>
+			    <?php $industrial_page_where_to_buy->the_post(); ?>
+                <a class="blocks__item" href="<?php the_permalink(); ?>">
+                    <div class="blocks__image-bg" style="background-image: url('<?php echo kama_thumb_src('w=304 &h=290'); ?>')"></div>
+                    <div class="blocks__content">
+                        <header class="blocks__content-header">
+                            <img class="blocks__image_show"
+                                 src="<?php echo get_template_directory_uri(); ?>/site/assets/i/blocks/block-arrow.png">
+                            <img
+                                    class="blocks__image_hover"
+                                    src="<?php echo get_template_directory_uri(); ?>/site/assets/i/blocks/block-arrow-hover.png">
+                            <div class="blocsk__titles">
+                                <h4 class="blocks__brand-title"><?php echo carbon_get_the_post_meta('crb_where_to_buy_page_brand_title'.get_lang()); ?></h4>
+                                <h3 class="blocks__title"><?php echo carbon_get_the_post_meta('crb_where_to_buy_page_brand_subtitle'.get_lang()); ?></h3>
+                            </div>
+                        </header>
+                        <div class="blocks__text"><?php echo carbon_get_the_post_meta('crb_where_to_buy_page_short_text'.get_lang()); ?></div>
                     </div>
-                </div>
-            </a><a class="blocks__item" href="https://www.brother-ism.com/products/industrial-garment-printers.html">
-                <div class="blocks__image-bg" style="background-image: url('assets/i/blocks/2.jpg')"></div>
-                <div class="blocks__content">
-                    <header class="blocks__content-header"><img class="blocks__image_show"
-                                                                src="assets/i/blocks/block-arrow.png"><img
-                                class="blocks__image_hover" src="assets/i/blocks/block-arrow-hover.png">
-                        <div class="blocsk__titles">
-                            <h4 class="blocks__brand-title">Industrial</h4>
-                            <h3 class="blocks__title">Garment Printers</h3>
-                        </div>
-                    </header>
-                    <div class="blocks__text">Discover the Single Platen White ink <br> Direct-to-Garment Printer.</div>
-                </div>
-            </a></div>
-    </section>
+                </a>
+		    <?php endwhile; ?>
+		    <?php wp_reset_postdata(); ?>
+	    <?php endif; ?>
+
+	    <?php $garment_page_where_to_buy = new WP_Query([
+		    'page_id' => 112
+	    ]); ?>
+
+        <?php if($garment_page_where_to_buy->have_posts()): ?>
+        	<?php while($garment_page_where_to_buy->have_posts()): ?>
+        		<?php $garment_page_where_to_buy->the_post(); ?>
+                <a class="blocks__item" href="<?php the_permalink(); ?>">
+                    <div class="blocks__image-bg" style="background-image: url('<?php echo kama_thumb_src('w=304 &h=290'); ?>')"></div>
+                    <div class="blocks__content">
+                        <header class="blocks__content-header">
+                            <img class="blocks__image_show"
+                                 src="<?php echo get_template_directory_uri(); ?>/site/assets/i/blocks/block-arrow.png">
+                            <img
+                                    class="blocks__image_hover"
+                                    src="<?php echo get_template_directory_uri(); ?>/site/assets/i/blocks/block-arrow-hover.png">
+                            <div class="blocsk__titles">
+                                <h4 class="blocks__brand-title"><?php echo carbon_get_the_post_meta('crb_where_to_buy_page_brand_title'.get_lang()); ?></h4>
+                                <h3 class="blocks__title"><?php echo carbon_get_the_post_meta('crb_where_to_buy_page_brand_subtitle'.get_lang()); ?></h3>
+                            </div>
+                        </header>
+                        <div class="blocks__text"><?php echo carbon_get_the_post_meta('crb_where_to_buy_page_short_text'.get_lang()); ?></div>
+                    </div>
+                </a>
+	        <?php endwhile; ?>
+        	<?php wp_reset_postdata(); ?>
+        <?php endif; ?>
+
+    </div>
+</section>
 <?php get_footer(); ?>
